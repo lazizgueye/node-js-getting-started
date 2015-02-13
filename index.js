896066ce87d179ff607148c06073c744784dafd7
gueyel <lazizgueye@gmail.com>
2015-02-13 10:26:00 +0100

    index

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
+var package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+var version=package.version;
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! ('+version+')');
 });
 
 app.listen(app.get('port'), function() {
