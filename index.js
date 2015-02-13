9809a2d0f9a43ae0b2e0df4ab19dd5c742b3c300
gueyel <lazizgueye@gmail.com>
2015-02-13 10:33:37 +0100

    index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World! ('+version+')');
+  response.send('Hello World Zizou! ('+version+')');
 });
 
 app.listen(app.get('port'), function() {
